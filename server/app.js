7cbe205a6092c2d364aa675e8bf896e45586be18
parooldaiya88 <parooldaiya88@gmail.com>
2023-10-13 11:13:39 +0200

    Adjusted cors config

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://blood-share-frontend.onrender.com"],
     credentials: true, // such as cookies or HTTP authentication) to be included in CORS requests
   })
 );
